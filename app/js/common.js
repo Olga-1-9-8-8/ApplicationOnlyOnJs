eaf2332bbf0234c0d6b374152b40021fd82ffef5
Olga <Olga-1-9-8-8@yandex.ru>
2021-01-12 16:17:06 +0300

    Next commit

--- app/js/common.js
+++ app/js/common.js
@@ -2,10 +2,24 @@ const imageProductCard = document.querySelector('.product-card__image')
 
 zoomImage(imageProductCard);
 
-
 bindButtonReview();
 
+floatingLabelColorChange();
 
+function floatingLabelColorChange(){
+    let form = document.forms.review;
+    let elements = form.elements.input;
+    for(let elem of elements){
+        elem.onblur = function(){
+            if(elem.value){
+                elem.nextElementSibling.style.color = 'white';
+            }else{
+                elem.nextElementSibling.style.color = '';
+            }
+        }
+    }
+   
+}
 
 function zoomImage(img){
   
@@ -43,26 +57,27 @@ function bindButtonReview(){
 
 
 
-window.onload = function(){
-    let canvas = document.getElementById('canvas');
-    if(canvas){
-        let context = canvas.getContext('2d');
-        context.fillStyle = "rgba(33, 150, 243,0.8)";
-        context.strokeStyle = "rgba(33, 150, 243,0.8)";
-        context.lineWidth = 5;
-        context.shadowColor = "rgba(0, 0, 0,0.28)";
-        context.shadowOffsetY = 5;
-        context.shadowBlur = 10;
-        context.font = "20pt Comic sans ms"
-        context.textBaseline = "bottom";
-        context.fillText("Оценивай здесь!",180,80);
-        context.beginPath();
-        context.arc(101,120,100,1.7*Math.PI,1.2*Math.PI,true);
-        context.lineJoin = "round";
-        context.moveTo(54,15);
-        context.lineTo(18,61);
-        context.lineTo(20,61);
-        context.lineTo(50,50);
-        context.stroke();
-    }  
-}
+
+// window.onload = function(){
+//     let canvas = document.getElementById('canvas');
+//     if(canvas){
+//         let context = canvas.getContext('2d');
+//         context.fillStyle = "rgba(33, 150, 243,0.8)";
+//         context.strokeStyle = "rgba(33, 150, 243,0.8)";
+//         context.lineWidth = 5;
+//         context.shadowColor = "rgba(0, 0, 0,0.28)";
+//         context.shadowOffsetY = 5;
+//         context.shadowBlur = 10;
+//         context.font = "20pt Comic sans ms"
+//         context.textBaseline = "bottom";
+//         context.fillText("Оценивай здесь!",180,80);
+//         context.beginPath();
+//         context.arc(101,120,100,1.7*Math.PI,1.2*Math.PI,true);
+//         context.lineJoin = "round";
+//         context.moveTo(54,15);
+//         context.lineTo(18,61);
+//         context.lineTo(20,61);
+//         context.lineTo(50,50);
+//         context.stroke();
+//     }  
+// }
