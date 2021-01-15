5130665afe6ca0239e6440f4fbfc3f4a192d1fa5
Olga <Olga-1-9-8-8@yandex.ru>
2021-01-15 12:07:11 +0300

    style buttons

--- app/js/common.js
+++ app/js/common.js
@@ -77,26 +77,3 @@ function bindButtonReview(){
 
 
 
-// window.onload = function(){
-//     let canvas = document.getElementById('canvas');
-//     if(canvas){
-//         let context = canvas.getContext('2d');
-//         context.fillStyle = "rgba(33, 150, 243,0.8)";
-//         context.strokeStyle = "rgba(33, 150, 243,0.8)";
-//         context.lineWidth = 5;
-//         context.shadowColor = "rgba(0, 0, 0,0.28)";
-//         context.shadowOffsetY = 5;
-//         context.shadowBlur = 10;
-//         context.font = "20pt Comic sans ms"
-//         context.textBaseline = "bottom";
-//         context.fillText("Оценивай здесь!",180,80);
-//         context.beginPath();
-//         context.arc(101,120,100,1.7*Math.PI,1.2*Math.PI,true);
-//         context.lineJoin = "round";
-//         context.moveTo(54,15);
-//         context.lineTo(18,61);
-//         context.lineTo(20,61);
-//         context.lineTo(50,50);
-//         context.stroke();
-//     }  
-// }
