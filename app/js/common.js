60dff271ae8ac3f9d2fb0ba5b426fdad70ad304c
Olga <Olga-1-9-8-8@yandex.ru>
2021-02-20 21:11:46 +0300

    Add fetch request

--- app/js/common.js
+++ app/js/common.js
@@ -1,35 +1,64 @@
-const imageProductCard = findImageProductCard();
+//
+const imageProductCardEl = findImageProductCard();
 
-zoomImage(imageProductCard);
+let formEl = findForm();
+let inputsFormEl = findFormInputs(formEl);
+let textareaFormEl = findFormTextarea(formEl);
+let submitFormEl = findFormSubmit(formEl);
 
-bindButtonReview();
-
-floatingLabelColorChange();
-
-bindButtonFormSubmit();
-
-
-
+let buttonReviewEl = findButtonReviw();
+let newReviewEl = findNewReview();
 
 
+    
 
+zoomImage(imageProductCardEl);
 
+floatingLabelColorChange(inputsFormEl);
 
+bindButtonReview(buttonReviewEl,newReviewEl);
 
-
-
-
-
-
+bindButtonFormSubmit(formEl);
 
 
 
 
+
+
+
+//Функции которые находят элементы на странице
 
 function findImageProductCard(){
     return document.querySelector('.product-card__image');
 }
 
+function findForm(){
+    return document.forms.review;
+}
+
+function findFormInputs(form){
+ return form.elements.input;
+}
+
+function findFormTextarea(form){
+    return form.elements.textarea;
+}
+
+function findFormSubmit(form){
+    return form.elements.submit;
+}
+    
+function findButtonReviw(){
+    return document.querySelector('.reviews__button')
+}
+
+function findNewReview(){
+    return document.querySelector('.reviews__form-wrapper')
+}
+
+
+
+// Функция увеличения картинки
 function zoomImage(img){
   
     img.addEventListener('mousemove',function(e){
@@ -43,16 +72,18 @@ function zoomImage(img){
         // number/total *100 = %
         let positionX = (mouseX/width * 100);
         let positionY = (mouseY/height * 100);
-        imageProductCard.style.backgroundPosition = `${positionX}% ${positionY}%`
+        img.style.backgroundPosition = `${positionX}% ${positionY}%`
     })
     img.addEventListener('mouseleave',function(){
         img.style.backgroundPosition = 'center';
     })
 }
 
-function floatingLabelColorChange(){
-    let form = document.forms.review;
-    let elements = form.elements.input;
+
+
+
+//Функция изменения цвета лейбла флоата
+function floatingLabelColorChange(elements){
     for(let elem of elements){
         elem.onblur = function(){
             if(elem.value){
@@ -61,52 +92,59 @@ function floatingLabelColorChange(){
                 elem.nextElementSibling.style.color = '';
             }
         }
-    }
-   
+    }  
 }
 
-function bindButtonReview(){ 
-    let buttonReview = document.querySelector('.reviews__button');
-    let newReview = document.querySelector('.reviews__form-wrapper');
+
+//Закрыть-открыть форму отзыва
+function bindButtonReview(buttonReview,newReview){ 
     buttonReview.addEventListener('click',function(){
-    newReview.hidden = !newReview.hidden;
-    if(buttonReview.innerHTML =='Написать Отзыв'){
-        buttonReview.innerHTML = "Закрыть Отзыв"
-    } else {
-        buttonReview.innerHTML = "Написать Отзыв"
-    }
+        newReview.hidden = !newReview.hidden;
+        if(buttonReview.innerHTML =='Написать Отзыв'){
+            buttonReview.innerHTML = "Закрыть Отзыв"
+        } else {
+            buttonReview.innerHTML = "Написать Отзыв"
+        }
     })
 }
 
 
-function bindButtonFormSubmit(submitFormHandler){
-    let form = document.forms.review;
-    let inputs = form.elements.input;
-    let textarea = form.elements.textarea;
-    let submit = form.elements.submit;
 
-    form.addEventListener('submit',function(event){
+
+function bindButtonFormSubmit(form){
+    
+    form.addEventListener('submit',async function(event){
         event.preventDefault();
-        const reviewObj = {
-            name: inputs[0].value,
-            surname: inputs[1].value,
-            starScore: getRating(),
-            reviewText: textarea.value
-        }
-        console.log(reviewObj)
         
-        
+        //обязательно писать после адреса до базы, название своей коллекции.json
+        let response = await fetch('https://review-app-6ea6e-default-rtdb.firebaseio.com/reviews.json',{
+            method:'POST',
+            body: JSON.stringify(Object.fromEntries(new FormData(this))),
+            headers:{
+    	        'Content-Type': 'application/json'
+            }
+        });
+
+
+        let result = await response.json();
 
     })
 }  
 
-function getRating(){
-    let radios = document.getElementsByName('rating');
-    for(let radio of radios){
-        radio.onchange = function(){
+// function getRating(){
+//     let radios = document.getElementsByName('rating');
+//     for(let radio of radios){
+//         radio.onchange = function(){
             
-        }
-    }
- return alert('sdg');
-}
+//         }
+//     }
+//  return alert('sdg');
+// }
+// const reviewObj = {
+//     name: inputs[0].value,
+//     surname: inputs[1].value,
+//     starScore: getRating(),
+//     reviewText: textarea.value
+// }
+// console.log(reviewObj)
 
